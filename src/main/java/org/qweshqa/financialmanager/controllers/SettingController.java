6ba2ac659a39b89eef12b687231bd137ce3e3391
qweshqa <stasigrok2712@gmail.com>
2024-06-21 17:30:05 +0300

    add settings page, opportunity to change currency (currency convert WIP)

---
+++ src/main/java/org/qweshqa/financialmanager/controllers/SettingController.java
@@ -0,0 +1,60 @@
+package org.qweshqa.financialmanager.controllers;
+
+import org.qweshqa.financialmanager.models.Setting;
+import org.qweshqa.financialmanager.models.User;
+import org.qweshqa.financialmanager.services.SettingService;
+import org.qweshqa.financialmanager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.money.CurrencyUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/settings")
+public class SettingController {
+
+    private final SettingService settingService;
+
+    private final UserService userService;
+
+    @Autowired
+    public SettingController(SettingService settingService, UserService userService) {
+        this.settingService = settingService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/currency")
+    public String getCurrencySettings(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(authentication.getName()).get();
+
+        List<String> currencies = new ArrayList<>(); {
+            currencies.add("UAH");
+            currencies.add("EUR");
+            currencies.add("RUB");
+            currencies.add("USD");
+        }
+
+        currencies.remove(user.getSetting().getCurrencyUnit());
+
+        model.addAttribute("user_currency", user.getSetting().getCurrencyUnit());
+        model.addAttribute("currencies", currencies);
+        model.addAttribute("settings", new Setting());
+        model.addAttribute("settingsId", user.getSetting().getId());
+
+        return "/settings/currency";
+    }
+    @RequestMapping(value = "/{id}", method = {RequestMethod.PATCH, RequestMethod.POST})
+    public String saveCurrencySettings(@PathVariable("id") int id,
+                                       @ModelAttribute("settings") Setting setting){
+        settingService.updateCurrencySetup(id, setting);
+
+        return "redirect:/settings/currency?success";
+    }
+}
--- src/main/java/org/qweshqa/financialmanager/services/SettingService.java
+++ src/main/java/org/qweshqa/financialmanager/services/SettingService.java
@@ -3,7 +3,10 @@ package org.qweshqa.financialmanager.services;
 import org.qweshqa.financialmanager.models.Setting;
 import org.qweshqa.financialmanager.models.User;
 import org.qweshqa.financialmanager.repositories.SettingRepository;
+import org.qweshqa.financialmanager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +19,12 @@ public class SettingService {
 
     private final SettingRepository settingRepository;
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public SettingService(SettingRepository settingRepository) {
+    public SettingService(SettingRepository settingRepository, UserRepository userRepository) {
         this.settingRepository = settingRepository;
+        this.userRepository = userRepository;
     }
 
     public Optional<Setting> findSettingByUser(User user){
@@ -31,5 +37,11 @@ public class SettingService {
         setting.setCurrencyUnit(currencyUnit.getCurrencyCode());
 
         settingRepository.save(setting);
+    }
+
+    @Transactional
+    public void updateCurrencySetup(int settingsIdToUpdate, Setting updatedSettings){
+        Setting setting = settingRepository.findById(settingsIdToUpdate).get();
+        setting.setCurrencyUnit(updatedSettings.getCurrencyUnit());
     }
 }
