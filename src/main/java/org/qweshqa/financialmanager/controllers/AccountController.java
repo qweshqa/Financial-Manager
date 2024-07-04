a22b29cd5efcbe94d3739fd5d4bc0079ef62aaf1
qweshqa <stasigrok2712@gmail.com>
2024-07-04 23:56:44 +0300

    add account-view page

--- src/main/java/org/qweshqa/financialmanager/controllers/AccountController.java
+++ src/main/java/org/qweshqa/financialmanager/controllers/AccountController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/accounts")
@@ -36,6 +37,27 @@ public class AccountController {
         this.userService = userService;
         this.amountFormatter = amountFormatter;
         this.accountTypeStringConverter = accountTypeStringConverter;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String viewAccount(@PathVariable("id") int id, Model model){
+        Optional<Account> account = accountService.findById(id);
+
+        if(account.isEmpty()){
+            model.addAttribute("errorTitle", "404 Nothing found");
+            model.addAttribute("errorMessage", "Account with this id doesn't exist");
+            return "error";
+        }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(authentication.getName()).get();
+
+        model.addAttribute("user", user);
+        model.addAttribute("amountFormatter", amountFormatter);
+
+        model.addAttribute("account", account.get());
+
+        return "accounts/view";
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
